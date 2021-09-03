7e17719a0ff785c3dfe3e1dff2fb0e52ed469f14
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-02 20:39:10 -0400

    add boilerplate functions

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
+// functions
+function buildQuiz() {
+
+}
+
+function showResults() {
+
+}
+
+// display quiz right away
+buildQuiz();
+
+// show results on submit
+submitButton.addEventListener('click', showResults);
+
