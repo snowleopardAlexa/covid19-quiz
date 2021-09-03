d2749ee6f3af5cd048564a37308ffbe9733c07d3
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-02 21:13:58 -0400

    add build function, display all the questions

--- script.js
+++ script.js
@@ -1,98 +1,101 @@
+// FUNCTIONS
+function buildQuiz() {
+    // variable to store the html output
+    const output = [];
+   
+    // for each question
+    covidQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            // the code we want to run for each questions goes here
+            // variable to store the list of possible answers
+            const answers = [];
+            // and for each available answer - we're lopping here
+            for(letter in currentQuestion.answers) {
+                // add html radio button
+                answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionNumber}" value="${letter}">
+                            ${letter}:
+                            ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+            // add question and its answers to the output
+            output.push(
+                `<div class="question">${currentQuestion.question} </div>
+                 <div class="answers">${answers.join('')} </div>`
+            );
+         }
+      );
+      // combine output list into one string of html and put it on the page
+      quizContainer.innerHTML = output.join('');
+   }
+   
+   function showResults() {
+   
+   }
+
+// html elements and their references in VARIABLES
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 // quiz questions
 // object literals represent the individual questions
 // array holds all of the questions
 const covidQuestions = [
-   {
-       question: "What is the covid19 known commonly as a coronavirus?",
-       answers: {
-           a: "It is an infectious disease causd by the SARS-CoV-2 virus",
-           b: "It is a movie",
-           c: "It is a type of a chronic pain"
-       },
-       correctAnswer: "a"
-   },
-   {
-       question: "What are the symptoms of the covid19?",
-       answers: {
-           a: "No symptoms, it is not a disease",
-           b: "Fever, chills, cough, shortness of breath, fatique, muscle and body aches",
-           c: "Inflammation, the skin color changes to blue"
+    {
+        question: "What is the covid19 known commonly as a coronavirus?",
+        answers: {
+            a: "It is an infectious disease causd by the SARS-CoV-2 virus",
+            b: "It is a movie",
+            c: "It is a type of a chronic pain"
         },
-        correctAnswer: "b"
+        correctAnswer: "a"
     },
-   {
-       question: "When was covid19 discovered?",
+    {
+        question: "What are the symptoms of the covid19?",
+        answers: {
+            a: "No symptoms, it is not a disease",
+            b: "Fever, chills, cough, shortness of breath, fatique, muscle and body aches",
+            c: "Inflammation, the skin color changes to blue"
+         },
+         correctAnswer: "b"
+     },
+    {
+        question: "When was covid19 discovered?",
+        answers: {
+            a: "2023",
+            b: "1945",
+            c: "2019"
+        },
+        correctAnswer: "c"
+    },   
+    {
+        question: "How long does it take for symptoms of covid19 to appear after exposure?",
+        answers: {
+            a: "2 to 14 days",
+            b: "23 to 45 days",
+            c: "immediately"
+        },
+        correctAnswer: "a"
+    },
+    {
+       question: "What are the recommendations for someone who has symptoms of covid19?",
        answers: {
-           a: "2023",
-           b: "1945",
-           c: "2019"
-       },
-       correctAnswer: "c"
-   },   
-   {
-       question: "How long does it take for symptoms of covid19 to appear after exposure?",
-       answers: {
-           a: "2 to 14 days",
-           b: "23 to 45 days",
-           c: "immediately"
-       },
+         a: "stay at home, wear a mask when you're around the others, separate yourself from the others ",
+         b: "go to the concert",
+         c: "go to the mall to buy new clothes"
+     },
        correctAnswer: "a"
-   },
-   {
-      question: "What are the recommendations for someone who has symptoms of covid19?",
-      answers: {
-        a: "stay at home, wear a mask when you're around the others, separate yourself from the others ",
-        b: "go to the concert",
-        c: "go to the mall to buy new clothes"
-    },
-      correctAnswer: "a"
- }
-];
+  }
+ ];
 
-// html elements and their references in variables
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-// functions
-function buildQuiz() {
- // variable to store the html output
- const output = [];
-
- // for each question
- covidQuestions.forEach(
-     (currentQuestion, questionNumber) => {
-         // variable to store the list of possible answers
-         const answers = [];
-         // and for each available answer 
-         for(letter in currentQuestion.answers) {
-             // add html radio button
-             answers.push(
-                 `<label>
-                     <input type="radio" name="question${questionNumber}" value="${letter}">
-                         ${letter}:
-                         ${currentQuestion.answers[letter]}
-                 </label>`
-             );
-         }
-         // add question and its answers to the output
-         output.push(
-             `<div class="question">${currentQuestion.question} </div>
-              <div class="answers">${answers.join('')} </div>`
-         );
-      }
-   );
-   // combine output list into one string of html and put it on the page
-   quizContainer.innerHTML = output.join('');
-}
-
-function showResults() {
-
-}
-
-// display quiz right away
+// DISPLAY QUIZ right away
 buildQuiz();
 
-// show results on submit
+// EVENT LISTENERS - show results on submit
 submitButton.addEventListener('click', showResults);
+
+
+
 
