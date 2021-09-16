0cac8556a461f1cd9d5184242b1070a33d093e35
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-16 19:23:16 -0400

    add function show results

--- script.js
+++ script.js
@@ -32,8 +32,35 @@ function buildQuiz() {
    }
    
    function showResults() {
-   
-   }
+       // gather answer containers from quiz
+       const answerContainers = quizContainer.querySelectorAll('.answers');
+       // keep track of user's answers
+       let numCorrect = 0;
+
+       // for each question...
+       covidQuestions.forEach((currentQuestion, questionNumber) => {
+           // find selected answer
+           const answerContainer = answerContainers[questionNumber];
+           const selector = `input[name=question${questionNumber}]:checked`;
+           const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+           // if answer is correct
+           if(userAnswer === currentQuestion.correctAnswer) {
+               // add to the number of correct answers
+               numCorrect ++;
+
+               // color the answers green
+               answerContainer[questionNumber].style.color = 'green';
+           }
+           // if answer is wrong or blank
+           else {
+               // color the answers red
+               answerContainers[questionNumber].style.color = 'red';
+           }
+       });
+       // show number of correct answers out of total
+       resultsContainer.innerHTML = `${numCorrect} out of ${covidQuestion.length}`;
+    }
 
 // html elements and their references in VARIABLES
 const titleContainer = document.getElementById('title')
